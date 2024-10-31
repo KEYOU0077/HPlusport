141ab3547df6e1ffc497b8df4e63e6e726002d3c
Joshua Bassey <joshuabssy@gmail.com>
2024-10-31 11:22:16 +0100

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace HPlusport.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual List<Product> Products { get; set; }
+    }
+}
